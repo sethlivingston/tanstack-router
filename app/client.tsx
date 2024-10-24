862fe551ff139eacca86fa25eb6dbcbc9c84ef23
Seth Livingston <sethlivingston@users.noreply.github.com>
2024-10-24 19:53:17 +0000

    Demonstrate case sensitivity of router.basepath

--- app/client.tsx
+++ app/client.tsx
@@ -4,5 +4,6 @@ import { StartClient } from '@tanstack/start'
 import { createRouter } from './router'
 
 const router = createRouter()
+router.basepath = "CaseSensitive";
 
 hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
